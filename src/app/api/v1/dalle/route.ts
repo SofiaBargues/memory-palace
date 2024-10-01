0de27a5de72539f39c986730a4217200380acaf8
Sofia Bargues <barguessofia@gmail.com>
2024-10-01 17:32:07 +0100

    open ia

--- src/app/api/v1/dalle/route.ts
+++ src/app/api/v1/dalle/route.ts
@@ -1,3 +1,31 @@
+import { Configuration, OpenAIApi } from "openai";
+
 export async function GET() {
   return new Response("Hello from DALL-E!");
+}
+export async function POST(request: Request) {
+  try {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const openai = new OpenAIApi(configuration);
+
+    const { prompt } = await request.json();
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      // size: "1024x1024",
+      size: "256x256",
+
+      response_format: "b64_json",
+    });
+    // console.log(aiResponse);
+    const image = aiResponse.data.data[0].b64_json;
+
+    return Response.json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    return new Response(error.message, { status: 500 });
+  }
 }
