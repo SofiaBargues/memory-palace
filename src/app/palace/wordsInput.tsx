5512224a07b29ef84d10a05b81acbd25e9346c6f
Sofia Bargues <barguessofia@gmail.com>
2024-10-23 18:52:32 +0100

    submit

--- src/app/palace/wordsInput.tsx
+++ src/app/palace/wordsInput.tsx
@@ -12,22 +12,30 @@ export function WordsInput({
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }) {
   return (
-    <Card className="p-6 flex flex-col  gap-3 w-72">
-      <form onSubmit={handleSubmit} className="flex flex-col w-28">
-        {initialWords.map((x, index) => (
-          <div key={index} className="flex gap-3 items-center">
-            <p>{index + 1}</p>
-            <Input
-              type="text"
-              id="myInput"
-              className="border-2 w-52 p-1 m-2 bg-white rounded-lg h-8 "
-              placeholder={"Word"}
-              name={"input_" + index.toString()}
-            />
-          </div>
-        ))}
-        <Button type="submit">Submit</Button>
-      </form>
-    </Card>
+    <>
+      <Card className="p-6 flex flex-col  gap-3 w-72">
+        <form
+          id="submit"
+          onSubmit={handleSubmit}
+          className="flex flex-col w-28"
+        >
+          {initialWords.map((x, index) => (
+            <div key={index} className="flex gap-3 items-center">
+              <p>{index + 1}</p>
+              <Input
+                type="text"
+                id="myInput"
+                className="border-2 w-52 p-1 m-2 bg-white rounded-lg h-8 "
+                placeholder={"Word"}
+                name={"input_" + index.toString()}
+              />
+            </div>
+          ))}
+        </form>
+      </Card>
+      <Button form="submit" type="submit">
+        Submit
+      </Button>
+    </>
   );
 }
