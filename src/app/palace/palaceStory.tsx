24a6c57ecb9d8dccd802d9c881f80e1b6980ea62
Sofia Bargues <barguessofia@gmail.com>
2024-10-28 22:34:19 +0000

    carrusell

--- src/app/palace/palaceStory.tsx
+++ src/app/palace/palaceStory.tsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 import { StoryPart } from "./placePort";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Palace } from "../api/v1/generate/route";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export function PalaceStory({ palace }: { palace: Palace }) {
   const [part, setPart] = useState<number>(1);
@@ -11,37 +19,43 @@ export function PalaceStory({ palace }: { palace: Palace }) {
   };
   console.log(palace);
   return (
-    <ScrollArea className=" whitespace-nowrap rounded-md border ">
-      <div
-        className="flex w-max 
-      space-x-4 p-4"
-      >
-        <figure className="shrink-0">
-          <div className="overflow-hidden rounded-md ">
-            <StoryPart
-              narrative={palace.sentences.slice(0, 3).join(" ")}
-              image={palace.images[0]}
-            />
+    <Carousel className=" whitespace-nowrap rounded-md border ">
+      <CarouselPrevious />
+      <CarouselNext />
+      <CarouselContent>
+        <CarouselItem key="1">
+          <Card className="shrink-0">
+            <CardContent className="overflow-hidden rounded-md ">
+              <StoryPart
+                narrative={palace.sentences.slice(0, 3).join(" ")}
+                image={palace.images[0]}
+              />
+            </CardContent>
+          </Card>
+        </CarouselItem>
+        <CarouselItem key="2">
+          <Card className="shrink-0">
+            <CardContent className="overflow-hidden rounded-md">
+              <StoryPart
+                narrative={palace.sentences.slice(3, 7).join(" ")}
+                image={palace.images[1]}
+              />
+            </CardContent>
+          </Card>
+        </CarouselItem>{" "}
+        <CarouselItem key="3">
+          <div className="p-1">
+            <Card className="shrink-0">
+              <CardContent className="overflow-hidden rounded-md">
+                <StoryPart
+                  narrative={palace.sentences.slice(7).join(" ")}
+                  image={palace.images[2]}
+                />
+              </CardContent>
+            </Card>
           </div>
-        </figure>
-        <figure className="shrink-0">
-          <div className="overflow-hidden rounded-md">
-            <StoryPart
-              narrative={palace.sentences.slice(3, 7).join(" ")}
-              image={palace.images[1]}
-            />
-          </div>
-        </figure>
-        <figure className="shrink-0">
-          <div className="overflow-hidden rounded-md">
-            <StoryPart
-              narrative={palace.sentences.slice(7).join(" ")}
-              image={palace.images[2]}
-            />
-          </div>
-        </figure>
-      </div>
-      <ScrollBar orientation="horizontal" />
-    </ScrollArea>
+        </CarouselItem>
+      </CarouselContent>
+    </Carousel>
   );
 }
