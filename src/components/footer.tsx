07b8750dec9d67435aa0cb285ef1d7a042cd8c0f
Sofia Bargues <barguessofia@gmail.com>
2024-12-11 16:27:35 +0000

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,10 @@ import { LinkedIn } from "./LinkedIn";
 
 export function Footer() {
   return (
-    <footer className="bg-black py-2 w-full">
-      <div className="flex justify-row justify-center items-center  container mx-auto px-4">
-        <p className="text-sm flex text-gray-200 text-center gap-2">
+    <footer className="bg-white py-2 w-full">
+      {/* w-full flex justify-between items-center bg-withe sm:px-89 px-4 py-4 border-b border-b-[#e6ebf4]  */}
+      <div className="flex justify-row justify-center items-center  container mx-auto  py-4 px-4 border-t border-t-[#e6ebf4]  ">
+        <p className="text-sm flex text- text-center gap-2">
           Sofia Bargues
           <LinkedIn />
           <GitHubRepo />
