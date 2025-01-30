105d43e2478ab5d461987687cb0e4148dddd4a8c
Sofia Bargues <barguessofia@gmail.com>
2025-01-30 08:15:53 +0000

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,11 +5,11 @@ export function Footer() {
   return (
     <footer className="bg-white py-2 w-full   px-4 border-t border-t-[#e6ebf4]">
       <div className="flex justify-row justify-center items-center  container mx-auto  ">
-        <p className="text-sm flex text- text-center gap-2">
-          Sofia Bargues
+        <div className="text-sm flex text- text-center gap-2">
+         <p>Sofia Bargues</p>
           <LinkedIn />
           <GitHubRepo />
-        </p>
+        </div>
       </div>
     </footer>
   );
