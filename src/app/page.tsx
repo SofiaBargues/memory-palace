012e612482c4883c525534b167a1f490680892d8
Sofia Bargues <barguessofia@gmail.com>
2025-02-25 17:18:10 -0300

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import ExperienceCard from "@/components/ui/experienceCards";
-import { CastleIcon, Footprints, PenTool } from "lucide-react";
+// import ExperienceCard from "@/components/ui/experienceCards";
+// import { CastleIcon, Footprints, PenTool } from "lucide-react";
 import CallToAction from "@/components/ui/cta";
 import Hero from "@/components/ui/hero";
 
